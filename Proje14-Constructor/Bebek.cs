685dae7ea9790bebc3c85fe8cce6d6216fed87eb
Eda Özge UĞURLU <edaozgeugurlu@gmail.com>
2024-10-27 09:55:21 +0300

    Proje dosyası ekle.

---
+++ Proje14-Constructor/Bebek.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje14_Constructor
+{
+    internal class Bebek
+    {
+        // Üye değişkenleri
+        public DateTime dogumtarihi;  // Bebeğin doğum tarihi
+        public string ad;             // Bebeğin adı
+        public string soyad;          // Bebeğin soyadı
+
+
+        // DogumTarihi özelliği
+        public DateTime DogumTarihi
+        {
+            get
+            {
+                return dogumtarihi;
+            }
+            set
+            {
+                dogumtarihi = value;
+            }
+        }
+
+        // Ad özelliği
+        public string Ad
+        {
+            get
+            {
+                return ad;
+            }
+            set
+            {
+                ad = value;
+            }
+        }
+
+        // Soyad özelliği
+        public string Soyad
+        {
+            get
+            {
+                return soyad;
+            }
+            set
+            {
+                soyad = value;
+            }
+        }
+
+
+        // Varsayılan yapıcı (constructor)
+        public Bebek()
+        {
+            dogumtarihi = DateTime.Now;  // Doğum tarihini mevcut zaman olarak ayarla
+            BebekAglaması();             // Bebek ağlama sesini çıkar
+        }
+
+        // Parametreli yapıcı (constructor)
+        public Bebek(string ad, string soyad)
+        {
+            // Sınıfın üye değişkeni 'ad' ile parametre 'ad' arasındaki karışıklığı önler.
+            this.ad = ad;                // Bebeğin adını ayarla
+            this.soyad = soyad;          // Bebeğin soyadını ayarla
+            dogumtarihi = DateTime.Now;  // Doğum tarihini mevcut zaman olarak ayarla
+            BebekAglaması();             // Bebek ağlama sesini çıkar
+
+        }
+
+        // Bebeğin bilgilerini yazdıran metot
+        public void BabyInfo()
+        {
+            Console.WriteLine($"Adı: {ad}, \nSoyadı: {soyad}, \nDoğum Zamanı: {dogumtarihi}");
+        }
+
+        // Bebek ağlama sesi çıkaran metot
+        public void BebekAglaması()
+        {
+            Console.WriteLine("Ingaaaa Ingaaaaaaaaaa!!!");// Ağlama sesini yazdır
+        }
+
+    }
+}
+
+
+
---
+++ Proje14-Constructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using Proje14_Constructor; // Proje içinde tanımlı olan Bebek sınıfını kullanmak için eklenir.
+using System; // Sistem kütüphanesini kullanmak için eklenir.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Varsayılan yapıcı ile yeni bir Bebek nesnesi oluştur
+        Bebek bebek1 = new Bebek();
+
+        // Bebek1'in adını ve soyadını ayarla
+        bebek1.ad = "Eda";
+        bebek1.soyad = "ÖZGE";
+
+        // Bebek1'in bilgilerini yazdır
+        bebek1.BabyInfo();
+
+        Console.WriteLine();
+
+        // Parametreli yapıcı ile yeni bir Bebek nesnesi oluştur
+        Bebek bebek2 = new Bebek("Ali", "ATABAK");
+
+        // Bebek2'nin bilgilerini yazdır
+        bebek2.BabyInfo();
+
+
+        Console.ReadKey();
+    }
+}
+
